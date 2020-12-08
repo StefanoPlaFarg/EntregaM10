d460ca8834254b394a35c9c308eaddc3ba1dced4
Stefano <stefano.pola.fargas@gmail.com>
2020-12-08 20:05:45 +0100

    Creacio del metode retornaLlistaStrings

--- src/MainApp.java
+++ src/MainApp.java
@@ -22,24 +22,40 @@ public class MainApp {
 	public static void main(String[] args) {
 		
 		List<String> stringList = new ArrayList<String>();
-		List<String> auxList = new ArrayList<String>();
+		
 		
 		stringList.add("Pau");
 		stringList.add("Marc");
 		stringList.add("Ari");
 		stringList.add("Albert");
+		stringList.add("Ash");
+		stringList.add("Ara");
+		
+		retornaLlistaStrings(stringList);
 		
 		
-		stringList.forEach(s->{
-			if (s.substring(0, 1).equals("A")&& (s.length()==3)) auxList.add(s);}
-				
-			);
 		
-		auxList.forEach(s->System.out.println(s));
 	
 	}
 	
-	    
+	public static List<String> retornaLlistaStrings(List<String> stringList) {
+
+		List<String> list = stringList;
+
+		List<String> auxList = new ArrayList<String>();
+
+		list.forEach(s -> {
+			if (s.substring(0, 1).equals("A") && (s.length() == 3))
+				auxList.add(s);
+		}
+
+		);
+
+		auxList.forEach(s -> System.out.println(s));
+
+		return auxList;
+
+	}
 	
 
 }
