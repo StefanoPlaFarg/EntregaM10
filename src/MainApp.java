a7aa31eb52aaeb1343c1f5e6407a5ff4a29ec118
Stefano <stefano.pola.fargas@gmail.com>
2020-12-08 19:55:46 +0100

    Creació de auxList i implementacio de lambdas

--- src/MainApp.java
+++ src/MainApp.java
@@ -22,13 +22,26 @@ public class MainApp {
 	public static void main(String[] args) {
 		
 		List<String> stringList = new ArrayList<String>();
-		stringList.add("Primer");
-		stringList.add("Segon");
-		stringList.add("Tercer");
-		stringList.add("Quart");
+		List<String> auxList = new ArrayList<String>();
 		
-		stringList.forEach(s-> if 
-
+		stringList.add("Pau");
+		stringList.add("Marc");
+		stringList.add("Ari");
+		stringList.add("Albert");
+		
+		
+		stringList.forEach(s->{
+			if (s.substring(0, 1).equals("A")&& (s.length()==3)) auxList.add(s);}
+				
+			);
+		
+		auxList.forEach(s->System.out.println(s));
+	
 	}
+	
+	    
+	
 
 }
+
+
