ec58aca82dae2bbc282bff7526b04679d5ede710
Stefano <stefano.pola.fargas@gmail.com>
2020-12-08 19:17:15 +0100

    Creacio de llista de Strings

--- src/MainApp.java
+++ src/MainApp.java
@@ -6,13 +6,28 @@
  * @author stefano
  *
  */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+
 public class MainApp {
 
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		List<String> stringList = new ArrayList<String>();
+		stringList.add("Primer");
+		stringList.add("Segon");
+		stringList.add("Tercer");
+		stringList.add("Quart");
+		
+		stringList.forEach(s-> if 
 
 	}
 
