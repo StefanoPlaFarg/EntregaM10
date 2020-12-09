50334762665f3dc62ab5bac7e14b5647c35cf042
Stefano <stefano.pola.fargas@gmail.com>
2020-12-09 22:29:25 +0100

    Creacio del metode retornaLlistaStringsLambda i retornaLlistaStringsStreams

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,3 +1,4 @@
+
 /**
  * 
  */
@@ -8,46 +9,52 @@
  */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.Collections;
-
 
 public class MainApp {
 
-	
-	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		List<String> stringList = new ArrayList<String>();
-		
-		
-		stringList.add("Pau");
-		stringList.add("Marc");
-		stringList.add("Ari");
-		stringList.add("Albert");
-		stringList.add("Ash");
-		stringList.add("Ara");
-		stringList.add("Oriol");
-		stringList.add("Joan");
-		
-		retornaLlistaStrings(stringList);
-		
-		
-		
-	
-	}
-	
-	public static List<String> retornaLlistaStrings(List<String> stringList) {
 
-		List<String> list = stringList;
+		List<String> stringList = Arrays.asList("peter", "anna", "mike", "Oriol", "Joan", "xenia", "Ari", "Ash", "Ara");
+		System.out.println("Llista de noms: " + stringList);
+
+		System.out.println("Execució programa amb Lambdas. Llista de noms filtrats:");
+		Test testLambdas = s -> s.contains("o") && s.length() >= 5;
+		retornaLlistaStringsLambda(stringList, testLambdas);
+
+		System.out.println("Execució programa amb Streams i Lambdas. Llista de noms filtrats:");
+		Test testStreams = s -> s.contains("o") && s.length() >= 5;
+		retornaLlistaStringsStreams(stringList, testStreams);
+
+	}
+
+	public static List<String> retornaLlistaStringsStreams(List<String> stringList, Test testStreams) {
+
+		List<String> filteredList = stringList.stream().filter(s -> testStreams.applyTest(s))
+
+				.map(s -> {
+					System.out.println(s);
+					return s;
+				})
+
+				.collect(Collectors.toList());
+
+		return filteredList;
+
+	}
+
+	public static List<String> retornaLlistaStringsLambda(List<String> stringList, Test test) {
 
 		List<String> auxList = new ArrayList<String>();
 
-		list.forEach(s -> {
-			if (s.contains("o") && s.length()>=5 )
+		stringList.forEach(s -> {
+			if (s.contains("o"))
 				auxList.add(s);
 		}
 
@@ -58,8 +65,15 @@ public class MainApp {
 		return auxList;
 
 	}
-	
+
+	@FunctionalInterface
+	public interface Test {
+		boolean applyTest(String paraula);
+	}
 
 }
+
+
+
 
 
