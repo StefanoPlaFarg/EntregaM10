43901db3e1d9daa716a848b727266d7e94879b8b
Stefano <stefano.pola.fargas@gmail.com>
2020-12-09 13:04:20 +0100

    Afegir functional interface com a parametre al metode retornaLlistaStrings

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
 /**
  * 
  */
@@ -6,58 +10,44 @@
  * @author stefano
  *
  */
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Collections;
-
-
 public class MainApp {
 
-	
-	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		List<String> stringList = new ArrayList<String>();
-		
-		
-		stringList.add("Pau");
-		stringList.add("Marc");
-		stringList.add("Ari");
-		stringList.add("Albert");
-		stringList.add("Ash");
-		stringList.add("Ara");
-		
-		retornaLlistaStrings(stringList);
-		
-		
-		
-	
-	}
-	
-	public static List<String> retornaLlistaStrings(List<String> stringList) {
 
-		List<String> list = stringList;
+		List<String> stringList = Arrays.asList("peter", "anna", "mike", "xenia", "Ari", "Ash", "Ara");
+
+		Test test = s -> s.substring(0, 1).equals("A") && (s.length() == 3);
+
+		retornaLlistaStrings(stringList, test);
+
+	}
+
+	public static List<String> retornaLlistaStrings(List<String> stringList, Test test) {
 
 		List<String> auxList = new ArrayList<String>();
 
-		list.forEach(s -> {
-			if (s.substring(0, 1).equals("A") && (s.length() == 3))
+		stringList.forEach(s -> {
+			if (test.applyTest(s))
 				auxList.add(s);
-		}
-
-		);
-
+		});
 		auxList.forEach(s -> System.out.println(s));
-
 		return auxList;
 
+		// if (s.substring(0, 1).equals("A") && (s.length() == 3))
+		// auxList.add(s);
+		// }
+
+		// );
+
 	}
-	
+
+	@FunctionalInterface
+	public interface Test {
+		boolean applyTest(String paraula);
+	}
 
 }
-
 
