dacb41760b7e5f627293ba7b6fcbdfde051d8059
Stefano <stefano.pola.fargas@gmail.com>
2020-12-09 21:30:03 +0100

    Creacio del metode retornaLlistaStringsStreams

--- src/MainApp.java
+++ src/MainApp.java
@@ -11,6 +11,9 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+
 
 import java.util.Collections;
 
@@ -23,13 +26,37 @@ public class MainApp {
 
 		List<Integer> integerList = Arrays.asList(1, 33, 4, 5, 25, 43);
 		System.out.println("Llista de valors: " + integerList);
+		
+		System.out.println("Execució programa amb Lambdas. Llista de valors filtrats:");
 		Test testLambdas = i -> i % 2 == 0;
+		retornaLlistaStringsLambda(integerList, testLambdas);
 
-		retornaLlistaEntersLambda(integerList, testLambdas);
+		System.out.println("Execució programa amb Streams i Lambdas. Llista de valors filtrats:");
+		Test testStreams = i -> i % 2 == 0;
+		retornaLlistaStringsStreams(integerList, testStreams);
+		
+	}
+   
+	public static List<String> retornaLlistaStringsStreams(List<Integer> integerList, Test testStreams) {
+
+		List<String> filteredList = new ArrayList<String>();
+
+		integerList.stream().forEach(i -> {
+			if (testStreams.applyTest(i))
+				filteredList.add("o" + String.valueOf(i) + ",");
+			else
+				filteredList.add("e" + String.valueOf(i) + ",");
+
+		});
+         
+		filteredList.forEach(s -> System.out.println(s));
+		
+		return filteredList;
 
 	}
-
-	public static List<String> retornaLlistaEntersLambda(List<Integer> integerList, Test testLambdas) {
+	
+	
+	public static List<String> retornaLlistaStringsLambda(List<Integer> integerList, Test testLambdas) {
 
 		List<String> auxList = new ArrayList<String>();
 
